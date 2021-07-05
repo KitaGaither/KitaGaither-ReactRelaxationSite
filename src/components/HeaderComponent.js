edb6cb16ecd974aef33113250d2ba6f475412ce8
Lakita Gaither <lakitagaither@yahoo.com>
2021-07-05 15:50:13 -0500

    Intro to Redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,21 +1,29 @@
 import React, { Component }  from 'react';
-import { Button, Navbar, Jumbotron, NavbarToggler, Collapse, Nav, NavItem } from 'reactstrap';
+import { Button, Navbar, Jumbotron, NavbarToggler, Collapse, Nav, NavItem, Modal, ModalBody, ModalHeader } from 'reactstrap';
 import { NavLink } from 'react-router-dom';
+import { LocalForm, Control, Errors } from 'react-redux-form';
 
 class Header extends Component {
     constructor(props) {
         super(props);
 
-        this.toggleNav = this.toggleNav.bind(this);
         this.state = {
-          isNavOpen: false
+          isModalOpen: false
         };
+        this.toggleModal = this.toggleModal.bind(this);
+
     }
 
-    toggleNav() {
+    handleSubmit(values) {
+        console.log("Current state is: " + JSON.stringify(values));
+        alert("Current state is: " + JSON.stringify(values));
+    }
+
+    toggleModal() {
         this.setState({
-            isNavOpen: !this.state.isNavOpen
+            isModalOpen: !this.state.isModalOpen
         });
+        //alert('toggleModal Works but I dont know what is preventing my modal from openiing.');
     }
 
     render() {
@@ -40,6 +48,74 @@ class Header extends Component {
                             Book Now!
                         </Button>
                         </div>
+
+                        <div>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+
+                        <ModalBody>
+
+                            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+
+                                <div className="form-group">
+                                    <Label htmlFor="rating">Rating</Label>
+                                        <Control.select className="form-control" model=".rating" id="rating" name="rating">
+                                            <option value="option1">1</option>
+                                            <option value="option2">2</option>
+                                            <option value="option3">3</option>
+                                            <option value="option4">4</option>
+                                            <option value="option5">5</option>
+                                        </Control.select> 
+                                </div>
+
+                                <div className="form-group">       
+                                        <Label htmlFor="author">Your Name</Label>
+                                        <Control.text className="form-control" model=".author" id="author" name="author"
+                                            validators={{
+                                                required,
+                                                minLength: minLength(2),
+                                                maxLength: maxLength(15)
+                                            }}
+                                        />
+
+                                        <Errors
+                                        className="text-danger"
+                                        model=".author"
+                                        show="touched"
+                                        component="div"
+                                        messages={{
+                                            required: 'Required',
+                                            minLength: 'Must be at least 2 characters',
+                                            maxLength: 'Must be 15 characters or less'
+                                        }}
+                                        />
+                                        
+                                </div> 
+
+                                <div className="form-group">
+                                        <Label htmlFor="text">Comment</Label>
+                                        <Control.textarea className="form-control" rows="6" model=".text" id="text" name="text"
+                                        />
+                                </div>
+
+                                <div>
+                                <Button onClick={this.submitForm} type="submit" value="submit" color="primary">Submit
+                                </Button>
+                                </div>
+
+                            </LocalForm>
+
+                        </ModalBody>
+
+                </Modal>
+                
+                <Button onClick={this.toggleModal} outline color="secondary"><i className="fa fa-pencil fa-lg" aria-hidden="true"></i>
+                Submit Comment
+                </Button>  
+
+            </div>    
+
                     </div>
                 </Jumbotron>
                 
